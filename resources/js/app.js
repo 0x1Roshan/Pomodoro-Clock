1ec795f832dd0564b75a106570bcfe44368d35e8
Roshan <rcroshan63@gmail.com>
2017-07-28 08:05:08 +0545

    made countdown clock working

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,24 @@
 new Vue({
 	el: '#app',
-	data: {},
-	methods: {}
+	data: {
+		minutes: 24,
+		seconds: 59,
+	},
+	methods: {
+		start: function () {
+			setInterval(this.countDown, 1000);
+		},
+		countDown: function () {
+			if (this.minutes == 0 && this.seconds == 0) {
+				return;
+			}
+
+			if (this.seconds == 0) {
+				this.seconds = 59;
+				this.minutes--;
+			} else {
+				this.seconds--;
+			}
+		}
+	}
 });
