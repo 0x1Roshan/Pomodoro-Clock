4125d011653dd5e574aad64cedbc760ead43f1db
Roshan <rcroshan63@gmail.com>
2017-07-29 21:48:44 +0545

    updated js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 new Vue({
 	el: '#app',
 	data: {
-		minutes: 24,
-		seconds: 59,
+		minutes: 25,
+		seconds: 00,
 		running: false
 	},
 	methods: {
@@ -15,16 +15,25 @@ new Vue({
 			}
 		},
 		countDown: function () {
-			if (this.minutes == 0 && this.seconds == 0) {
+			if (this.minutes == 00 && this.seconds == 00) {
 				return;
 			}
 
 			if (this.seconds == 0) {
-				this.seconds = 59;
-				this.minutes--;
+				var seconds = this.makeTwoDigit(59);
+				this.seconds = seconds;
+
+				var minutes = this.makeTwoDigit(this.minutes);
+				this.minutes = minutes - 1;
 			} else {
-				this.seconds--;
+				var seconds = this.makeTwoDigit(this.seconds);
+				this.seconds = seconds - 1;
 			}
+		},
+		makeTwoDigit: function (number) {
+		 	number = ("0" + number).slice(-2);
+
+			return parseInt(number);
 		}
 	}
 });
