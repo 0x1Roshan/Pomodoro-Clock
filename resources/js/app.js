0c3481e7c4b098274531b49bd5802c886060d3fb
Roshan <rcroshan63@gmail.com>
2017-10-23 23:19:23 +0545

    updated logic

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ new Vue({
 	el: '#app',
 	data: {
 		minutes: 25,
-		seconds: 00,
+		seconds: 0,
 		running: false,
 		breakMinutes: 5,
 		breakSeconds: 0,
@@ -13,69 +13,95 @@ new Vue({
 		startPomodoro: function () {
 			if (!this.running) {
 				this.running = true;
-				setInterval(this.countDown("pomodoro"), 1000);
+				timer = setInterval(this.countDown, 1000, "pomodoro");
 			} else {
 				return;
 			}
 		},
-		startBreak: function () {
-			setInterval(this.countDown("break"), 1000);
+		startBreak: function (prevBreakMinutes) {
+			setInterval(this.countDown, 1000, "break", prevBreakMinutes);
 		},
-		countDown: function (type) {
-			if (this.minutes == 00 && this.seconds == 00) {
-				if (type=="pomodoro") {
-					this.running = false;
-					this.startBreak;
-				} else if (type=="break"){
-					this.running = true;
-					this.startPomodoro;
+		countDown: function (type, prevBreakMinutes) {
+			if ( type == "pomodoro" ) {
+				if ( this.seconds == 0 ) {
+					if ( this.minutes != 0 ) {
+						var seconds = this.makeTwoDigit( 59 );
+						this.seconds = seconds;
+
+						var minutes = this.makeTwoDigit( this.minutes );
+						this.minutes = minutes - 1;
+					} else {
+						this.minutes = this.lengthMinutes;
+						this.seconds = this.lengthSeconds;
+						this.running = false;
+						clearInterval(timer);
+					}
 				} else {
-					return;
+					var seconds = this.makeTwoDigit( this.seconds );
+					this.seconds = seconds - 1;
 				}
 			}
 
-			if (this.seconds == 0) {
-				var seconds = this.makeTwoDigit(59);
-				this.seconds = seconds;
+			if ( type=="break" ) {
+				if (this.breakMinutes == 0 && this.breakSeconds == 0 ) {
+					this.breakMinutes = prevBreakMinutes;
 
-				var minutes = this.makeTwoDigit(this.minutes);
-				this.minutes = minutes - 1;
-			} else {
-				var seconds = this.makeTwoDigit(this.seconds);
-				this.seconds = seconds - 1;
+					this.running = false;
+					return;
+				}
+
+				if ( this.breakSeconds == 0 ) {
+					var seconds = this.makeTwoDigit( 59 );
+					this.breakSeconds = seconds;
+
+					var minutes = this.makeTwoDigit( this.breakMinutes );
+					this.breakMinutes = minutes - 1;
+				} else {
+					var seconds = this.makeTwoDigit( this.breakSeconds );
+					this.breakSeconds = seconds - 1;
+				}
 			}
 		},
-		makeTwoDigit: function (number) {
-		 	number = ("0" + number).slice(-2);
 
-			return parseInt(number);// [FIXME RC]parseInt doesnot working as expected
+		makeTwoDigit: function (number) {
+		 	number = ("0" + number).slice( -2 );
+
+			return parseInt( number );// [FIXME RC]parseInt isnot working as expected
 		},
+
 		minusBreakMinutes: function () {
 			if (this.breakMinutes == 0) {
 				return;
 			}
+
 			this.breakMinutes--;
 		},
+
 		plusBreakMinutes: function () {
-			if (this.breakMinutes == 10) {
+			if ( this.breakMinutes == 10 ) {
 				return;
 			}
+
 			this.breakMinutes++;
 		},
+
 		minusLengthMinutes: function () {
 			if (this.lengthMinutes == 0) {
 				return;
 			}
+
 			this.lengthMinutes--;
 
 			if (!this.running) {
 				this.minutes = this.lengthMinutes;
 			}
 		},
+
 		plusLengthMinutes: function () {
 			if (this.lengthMinutes == 25) {
 				return;
 			}
+
 			this.lengthMinutes++;
 
 			if (!this.running) {
@@ -83,6 +109,7 @@ new Vue({
 			}
 		}
 	},
+
 	mounted: function () {
 		this.minutes = this.lengthMinutes;
 	}
